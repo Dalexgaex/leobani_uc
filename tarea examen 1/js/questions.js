7547260ab063774fb7ecca9a7aa5393dbac2c6f6
Dalexgaex <132409216+Dalexgaex@users.noreply.github.com>
2024-05-21 12:14:07 -0600

    Add files via upload

---
+++ tarea examen 1/js/questions.js
@@ -0,0 +1,59 @@
+
+let questions = [
+    {
+    numb: 1,
+    question: "¿Parte de una pc que se encargar en porcesar y pintar las texturas?",
+    answer: "Gpu",
+    options: [
+      "Procesador",
+      "Memoria ram",
+      "Tarjegt de red",
+      "Gpu"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Componete encargador en calcular las indicasiones de el usurio",
+    answer: "Cpu",
+    options: [
+      "Enfiramineto liquido",
+      "Cpu",
+      "Ssd",
+      "lector de discos"
+    ]
+  },
+    {
+    numb: 3,
+    question: "¿Que componete se encarga en almacenar los datos del usuario?",
+    answer: "HDD",
+    options: [
+      "Mmemoria cache",
+      "Memoria ram",
+      "La nube ",
+      "HDD"
+    ]
+  },
+    {
+    numb: 4,
+    question: "¿como se llama la memoria que se borra al apagar el pc?",
+    answer: " Memoria Ram",
+    options: [
+      "Memoria Ram",
+      "disco ssd",
+      "Memoria cahe",
+      "ninguna memoria se elimina"
+    ]
+  },
+    {
+    numb: 5,
+    question: "que pasa si un componete si se eleva mucho la temperatura pero sigue funciondo",
+    answer: "termar trading",
+    options: [
+      "suelta humo",
+      "termar trading",
+      "deja de funcionar",
+      "es normal"
+    ]
+  },
+
+];
